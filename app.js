4354d4f8dc37ef36f1355cdeee86da5aab609390
Black Ace <baoanh2003199@gmail.com>
2020-06-21 15:17:07 +0700

    Login & Register View : Done

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.all('/', function(req, res, next){
 // Route
 app.use('/admin', require('./routes/admin/home.route'));
 
+app.get('/register',function (req, res) {
+  res.render('register');
+});
+
+app.get('/login',function (req, res) {
+  res.render('login');
+});
 
 
 
