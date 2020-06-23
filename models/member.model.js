63fc85eb1739abb51b0fcbc671aa6250f654385f
Alan luu <luumanhquan.91@gmail.com>
2020-06-23 14:09:52 +0700

    them model phan member nguoi su dung them cac chuc nang xem list sua xoa xem thong tin chi tiet member

---
+++ models/member.model.js
@@ -0,0 +1,33 @@
+const db = require('../utils/db');
+
+const TBL_MEMBER = 'member';
+
+module.exports = {
+    all: function() {
+        return db.load(`select * from ${TBL_MEMBER}`);
+    },
+
+    add: function(entity) {
+        return db.insert(TBL_MEMBER, entity);
+    },
+
+    del: function(idMem) {
+        const condition = {
+            id: idMem,
+        };
+        return db.delete(TBL_MEMBER, condition);
+    },
+
+    update: function(entity) {
+        const condition = {
+            id: entity.id,
+        };
+
+        delete entity.id;
+        return db.update(TBL_MEMBER, entity, condition);
+    },
+
+    view: function(id) {
+        return db.load(`select * from ${TBL_MEMBER} where id=${id}`);
+    },
+};
