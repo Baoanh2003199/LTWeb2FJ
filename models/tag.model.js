c30707ff84de6cbfe84a18eee96248e824405571
Alan luu <luumanhquan.91@gmail.com>
2020-06-23 13:50:05 +0700

    push len phan model tag cac chuc nang them xoa sua xem bai viet

---
+++ models/tag.model.js
@@ -0,0 +1,32 @@
+const db = require('../utils/db');
+
+const TBL_TAG = 'tag';
+
+module.exports = {
+    all: function() {
+        return db.load(`select * from ${TBL_TAG}`);
+    },
+
+    add: function(entity) {
+        return db.insert(TBL_TAG, entity);
+    },
+
+    del: function(idTag) {
+        const condition = {
+            id: idTag,
+        };
+        return db.delete(TBL_TAG, condition);
+    },
+
+    update: function(entity) {
+        const condition = {
+            id: entity.id,
+        };
+        delete entity.id;
+        return db.update(TBL_TAG, entity, condition);
+    },
+
+    view: function(id) {
+        return db.load(`select * from ${TBL_TAG} where id=${id}`);
+    },
+};
