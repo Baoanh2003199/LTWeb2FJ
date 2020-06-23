82aec7e1c5e68677b7cd52c71b148e962001558c
Alan luu <luumanhquan.91@gmail.com>
2020-06-23 14:35:13 +0700

    them pham model subscriber them 3 chuc nang xem list view chi tiet xoa

---
+++ models/subscriber.model.js
@@ -0,0 +1,20 @@
+const db = require('../utils/db');
+
+const TBL_SUBSCRIBER = 'subscriber';
+
+module.exports = {
+    all: function() {
+        return db.load(`select * from ${TBL_SUBSCRIBER}`);
+    },
+
+    view: function(id) {
+        return db.load(`select * from ${TBL_SUBSCRIBER} where id=${id}`);
+    },
+
+    del: function(idSub) {
+        const condition = {
+            id: idSub,
+        };
+        return db.delete(TBL_SUBSCRIBER, condition);
+    },
+};
