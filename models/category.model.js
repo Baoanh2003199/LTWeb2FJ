484eec1b29147fdb74ffcd2938b441a49ec8e5c1
Alan luu <luumanhquan.91@gmail.com>
2020-06-23 13:24:30 +0700

    them model phan category model them xoa chinh sua xem id category

---
+++ models/category.model.js
@@ -0,0 +1,27 @@
+const db = require('../utils/db');
+
+const TBL_CATEGORY = 'category';
+
+module.exports = {
+    all: function() {
+        return db.load(`select * from ${TBL_CATEGORY}`);
+    },
+
+    add: function(entity) {
+        return db.insert(TBL_CATEGORY, entity);
+    },
+
+    del: function(idCat) {
+        const condition = {
+            id: idCat,
+        };
+        return db.delete(TBL_CATEGORY, condition);
+    },
+    update: function(entity) {
+        const condition = {
+            id: entity.id,
+        };
+        delete entity.id;
+        return db.patch(TBL_CATEGORY, entity, condition);
+    },
+};
