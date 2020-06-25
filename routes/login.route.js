8895909b851932b559a8388c6bda69b2ec50c69f
Black Ace <baoanh2003199@gmail.com>
2020-06-25 17:37:32 +0700

    Login route test

--- routes/login.route.js
+++ routes/login.route.js
@@ -7,48 +7,5 @@ route.get('/', redirectHome, function(req,res){
     res.render('login');
 })
 
-const redirectLogin = (req, res, next)=>{
-    if(!req.session.userId)
-    {
-        res.redirect('/login')
-    }
-    else
-    {
-        next();
-    }
-}
-
-
-const PermissionCheck = (req, res, next)=>{
-    if(!req.session.userId )
-    {
-  
-        res.redirect('/login')
-    }
-    else
-    {
-      if(req.session.role != 1)
-      {
-        res.redirect('/Error')
-      }
-      else
-      {
-          
-        next();
-      }
-        
-    }
-  }
-
-const redirectHome = (req, res, next)=>{
-    if(req.session.userId)
-    {
-        res.redirect('/')
-    }
-    else
-    {
-        next();
-    }
-}
 
 module.exports = route;
