4675c7ca752b662b0d1cae1c93942d9f510f9102
Black Ace <baoanh2003199@gmail.com>
2020-06-25 17:35:28 +0700

    Login route test

--- routes/login.route.js
+++ routes/login.route.js
@@ -2,29 +2,8 @@ const express = require('express');
 const route = express.Router();
 const bcrypt = require('bcrypt');
 
-route.post('/login', redirectHome, async function(req, res) {
-    const {username, password} = req.body
-    if(username && password)
-    {
-        const result = await LoginModel.findUser(username);
-        var hash = result[0].password
-        var validUser = bcrypt.compareSync(password, hash)
-        if(result)
-        {
-            var {userId, name, role} = req.session
-            req.session.userId = result[0].ID
-            req.session.name = result[0].username
-            req.session.role = result[0].role
-            res.redirect('/')
-        }
-        else
-        {
-            res.render('login',{isLoginFalse: true});
-        }
-    }
-})
 
-app.get('/', redirectHome, function(req,res){
+route.get('/', redirectHome, function(req,res){
     res.render('login');
 })
 
