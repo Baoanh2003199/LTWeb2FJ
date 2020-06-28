d89b17540e04657f0a6f3b9ca5a0d3e67b7aa2d7
Alan luu <luumanhquan.91@gmail.com>
2020-06-28 12:52:42 +0700

    them route phan show ra list news

--- routes/admin/news.route.js
+++ routes/admin/news.route.js
@@ -1,32 +1,33 @@
-const express = require('express')
-
+const express = require('express');
+const newModel = require('../../models/news.model');
 const route = express.Router();
 
 // Danh sách bài viết
-route.get('/', function(req, res){
-    return res.render('admin/news/home')
+route.get('/', async function(req, res) {
+    const list = await newModel.all();
+    res.render('admin/news/home', { news: list, empty: list.length === 0 });
 });
 // Thêm bài viết
-route.get('/add', function (req, res) {
-    return res.render('admin/news/add')
-})
+route.get('/add', function(req, res) {
+    return res.render('admin/news/add');
+});
 // Thêm bài viết
 // Lấy id tìm model rồi gán vào views
-route.get('/edit/:id', function (req, res) {
+route.get('/edit/:id', function(req, res) {
     const id = req.params['id'];
 
-    return res.render('admin/news/edit')
-})
+    return res.render('admin/news/edit');
+});
 // Duyệt bài
-route.get('/check', function (req, res) {
+route.get('/check', function(req, res) {
     const id = req.params['id'];
 
-    return res.render('admin/news/check')
-})
-// Xem chi tiết 
-route.get('/view/:id', function(req, res){
+    return res.render('admin/news/check');
+});
+// Xem chi tiết
+route.get('/view/:id', function(req, res) {
     const id = req.params['id'];
 
     res.render('admin/news/view');
-})
+});
 module.exports = route;
