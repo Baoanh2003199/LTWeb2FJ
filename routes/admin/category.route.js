97cd0c37bf71459431889e1bf70eace62fbe4df8
Alan luu <luumanhquan.91@gmail.com>
2020-06-28 13:18:31 +0700

    hoan thanh chuc nang xoa xem chi tiet

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -1,28 +1,37 @@
 const express = require('express');
-const catModel = require('../../models/category.model')
+const catModel = require('../../models/category.model');
 
 const route = express.Router();
 
 // Danh sách thể loại
-route.get('/',async function(req, res){
+route.get('/', async function(req, res) {
     const list = await catModel.all();
-    res.render('admin/category/home',{cat: list, empty: list.length === 0});
-})
+    res.render('admin/category/home', { cat: list, empty: list.length === 0 });
+});
 // Thêm thể loại
-route.get('/add', function(req, res){
+route.get('/add', function(req, res) {
     res.render('admin/category/add');
-})
-// Sửa thể loại theo id 
-route.get('/edit/:id', function(req, res){
-    // 
+});
+
+route.post('/add', async function(req, res) {});
+
+// Sửa thể loại theo id
+route.get('/edit/:id', function(req, res) {
+    //
     const id = req.params['id'];
 
     res.render('admin/category/edit');
-})
-// Xem chi tiết 
-route.get('/view/:id', function(req, res){
-    const id = req.params['id'];
+});
 
-    res.render('admin/category/view');
-})
+//delete
+route.post('/', async function(req, res) {
+    await catModel.del(req.body.id);
+    res.redirect('/admin/category');
+});
+// Xem chi tiết
+route.get('/view/:id', async function(req, res) {
+    const id = req.params['id'];
+    const list = await catModel.view(id);
+    res.render('admin/category/view', { cat: list, empty: list.length === 0 });
+});
 module.exports = route;
