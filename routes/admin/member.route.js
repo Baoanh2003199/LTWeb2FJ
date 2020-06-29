8c5870eae1c91d1744532e43790bfe23934c5339
Alan luu <luumanhquan.91@gmail.com>
2020-06-29 13:11:21 +0700

    them cac chuc nang them xoa cho phan member

--- routes/admin/member.route.js
+++ routes/admin/member.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const memberModel = require('../../models/member.model');
+const userModel = require('../../models/user.model');
 
 const route = express.Router();
 
@@ -11,14 +12,36 @@ route.get('/', async function(req, res) {
 
 // Thêm thành viên
 route.get('/add', function(req, res) {
-    return res.render('admin/member/add');
+    res.render('admin/member/add');
+});
+
+route.post('/add', async function(req, res) {
+    await memberModel.add(req.body);
+    res.redirect('admin/member');
+});
+
+route.post('/add', async function(req, res) {
+    await userModel.add(req.body);
+    res.redirect('admin/member');
+});
+
+//show user status list option
+route.get('/add', async function(req, res) {
+    const list = await userModel.all();
+    res.render('admin/member/add', { user: list });
 });
 
 // Xem chi tiết
 route.get('/view/:id', function(req, res) {
     const id = req.params['id'];
+    const list = memberModel.view(id);
+    res.render('admin/member/view', { member: list });
+});
 
-    return res.render('admin/member/view');
+//delete
+route.post('/delete/:id', async function(req, res) {
+    await memberModel.del(req.params.id);
+    res.redirect('/admin/member');
 });
 
 module.exports = route;
