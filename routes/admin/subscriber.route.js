f7e7a8d06c7442ec8964e078cc28e602b711d1cc
Alan luu <luumanhquan.91@gmail.com>
2020-06-29 16:15:49 +0700

    xuat danh sach cac doc gia da dang ky

--- routes/admin/subscriber.route.js
+++ routes/admin/subscriber.route.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const subscriberModel = require('../../models/subscriber.model');
 
 const route = express.Router();
 
-route.get('/', function(req, res){
-    return res.render('admin/subscriber/home');
-})
-route.get('/check', function(req, res){
+//list all
+route.get('/', async function(req, res) {
+    const list = await subscriberModel.all();
+    res.render('admin/subscriber/home', { sub: list });
+});
+
+route.get('/check', function(req, res) {
     return res.render('admin/subscriber/check');
-})
+});
+
+route.post('/delete/:id', async function(req, res) {
+    const id = req.params.id;
+    await subscriberModel.del(id);
+    res.redirect('/admin/subscriber');
+});
 
 module.exports = route;
