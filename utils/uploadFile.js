dc45572bd454d9c18018be1e0e87223ff9d2302e
Alan luu <luumanhquan.91@gmail.com>
2020-07-04 17:39:34 +0700

    chinh sau lai phan news

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,29 +1,29 @@
-const multer = require("multer");
-const md5 = require("md5");
+const multer = require('multer');
+const md5 = require('md5');
 
 const diskStorage = multer.diskStorage({
-    destination: function(req, file, callback){
+    destination: function(req, file, callback) {
         console.log(req.body);
-        const path = req.query.path
-        return callback(null, `uploads/${path}`);
+        const path = req.query.path;
+        return callback(null, `../public/uploads//${path}`);
     },
-    filename: function (req, file, callback) {
+    filename: function(req, file, callback) {
         console.log(req.body);
-      const match = ["img/png", "image/jpeg"];
-      if(match.indexOf(file.mimetype) == -1){        
-        const errorMsg = "Vui lòng chọn file hình ảnh";
-        return callback(errorMsg, null);
-      }
-      const mimetype = file.mimetype;
-      const extension = mimetype.replace("image/","");
-      const filename = md5(`${Date.now}${file.originalname}`);
-      return callback(null, `${filename}.${extension}`);
-    }
-  });
-  const uploadFile = multer({
-    storage:diskStorage
-  }).single("file");
+        const match = ['img/png', 'image/jpeg'];
+        if (match.indexOf(file.mimetype) == -1) {
+            const errorMsg = 'Vui lòng chọn file hình ảnh';
+            return callback(errorMsg, null);
+        }
+        const mimetype = file.mimetype;
+        const extension = mimetype.replace('image/', '');
+        const filename = md5(`${Date.now}${file.originalname}`);
+        return callback(null, `${filename}.${extension}`);
+    },
+});
+const uploadFile = multer({
+    storage: diskStorage,
+}).single('file');
 
-  module.exports = {
-      uploadFile : uploadFile
-  }
+module.exports = {
+    uploadFile: uploadFile,
+};
