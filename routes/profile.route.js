5e60751b88b87b8d3482ef77d950acca638509a8
Black Ace <baoanh2003199@gmail.com>
2020-07-15 16:20:40 +0700

    Icon update

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const regModel = require('../models/user.model');
 const subModel = require('../models/subscriber.model');
+const memModel = require('../models/member.model');
 const { check, validationResult } = require('express-validator');
 
 route.get('/', function(req, res){
