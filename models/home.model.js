4a5253b7127523e632d5e01d6406e0d5d744037f
Alan luu <luumanhquan.91@gmail.com>
2020-07-15 10:10:43 +0700

    lay ra bai ivet duoc xem nhieu nhat theo chuyen muc

--- models/home.model.js
+++ models/home.model.js
@@ -27,7 +27,9 @@ module.exports = {
         return db.load(`select * from ${TBL_NEWS} where id=${idNews}`);
     },
 
-    ManyNews: function() {
-        return db.load(`select * from ${TBL_NEWS} order by views limit 10`);
+    ManyNews: function(catID) {
+        return db.load(
+            `select * from ${TBL_NEWS} where catID=${catID}  order by views limit 10`
+        );
     },
 };
