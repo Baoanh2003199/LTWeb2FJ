26d6f946377ceaeef7cf9927156732a00f3b4dd4
Alan luu <luumanhquan.91@gmail.com>
2020-07-16 22:10:48 +0700

    then vao post avatar

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -5,27 +5,42 @@ const subModel = require('../models/subscriber.model');
 const memModel = require('../models/member.model');
 const roleModel = require('../models/role.model');
 const { check, validationResult } = require('express-validator');
-const DATE_FORMATER = require( 'dateformat' );
+const DATE_FORMATER = require('dateformat');
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './public/avatar/');
+    },
+    filename: function(req, file, cb) {
+        cb(
+            null,
+            file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+        );
+    },
+});
+const upload = multer({ storage: storage });
 
-route.get('/',async function(req, res){
+route.get('/', async function(req, res) {
     const subRes = await subModel.view(res.locals.userId);
     const user = await userModel.view(res.locals.userId);
-    const roleRes = await roleModel.single(user[0].roleId)
+    const roleRes = await roleModel.single(user[0].roleId);
     const obj = {
         username: user[0].username,
         name: subRes[0].name,
-        dob: DATE_FORMATER( subRes[0].dob, "yyyy-mm-dd"),
+        dob: DATE_FORMATER(subRes[0].dob, 'yyyy-mm-dd'),
         email: subRes[0].email,
         phone: subRes[0].phone,
-        expired: DATE_FORMATER( subRes[0].expired, "HH:MM:ss - dd/mm/yyyy"),
-        role: roleRes[0].name
-    }
-    res.render('profile/personal_infor',{user: obj});
+        expired: DATE_FORMATER(subRes[0].expired, 'HH:MM:ss - dd/mm/yyyy'),
+        role: roleRes[0].name,
+    };
+    res.render('profile/personal_infor', { user: obj });
 });
 
-route.post('/', async function(req, res){
-
-})
+route.post('/', upload.single('avatar'), async function(req, res) {
+    //dung cai nay req.file.filename, de
+    //lay ten luu vao cot avarta
+});
 
 module.exports = route;
