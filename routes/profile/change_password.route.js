0988f25b191d82e4c61fcefb909f63a33cf0fb70
Black Ace <baoanh2003199@gmail.com>
2020-07-21 08:59:53 +0700

    Fix change password bug

--- routes/profile/change_password.route.js
+++ routes/profile/change_password.route.js
@@ -11,7 +11,7 @@ route.get('/',function(req, res)
 });
 
 route.post('/',[
-    check('currpassword', 'Mật khẩu phải hơn 6 kí tự').isLength({ min: 6 }),
+    check('currpassword', 'Mật khẩu hiện tại phải hơn 6 kí tự').isLength({ min: 6 }),
     check('newpassword', 'Mật khẩu mới phải hơn 6 kí tự').isLength({ min: 6 }).custom((val, { req}) => {
         if (val !== req.body.cnewpassword) {
             throw new Error("Mật khẩu mới và xác nhận mật khẩu mới không khớp");
@@ -23,31 +23,35 @@ route.post('/',[
     })
   ],async function(req, res)
 {
-    const QueryObj = await userModel.view(res.locals.userId);
-    const pw = req.body.currpassword;
-    var hashed = QueryObj[0].password;
-    var validUser = bcrypt.compareSync(pw, hashed)
-    if(validUser)
-    {
-        const entity={
-            id: QueryObj[0].id,
-            username: QueryObj[0].username,
-            password: req.body.newpassword,
-            roleId: QueryObj[0].roleId,
-            status: QueryObj[0].status,
-        }
-        const result = await userModel.hashUpdate(entity);
-        if(result)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render('profile/change_password', {Error: errors.array(), isError: true });
+    }
+    else{
+        const QueryObj = await userModel.view(res.locals.userId);
+        const pw = req.body.currpassword;
+        var hashed = QueryObj[0].password;
+        var validUser = bcrypt.compareSync(pw, hashed)
+        if(validUser)
         {
-            res.redirect("/profile");
+            const entity={
+                id: QueryObj[0].id,
+                username: QueryObj[0].username,
+                password: req.body.newpassword,
+                roleId: QueryObj[0].roleId,
+                status: QueryObj[0].status,
+            }
+            const result = await userModel.hashUpdate(entity);
+            if(result)
+            {
+                res.redirect("/profile");
+            }
+        }
+        else
+        {
+            res.render('profile/change_password',{wrongPw:true})
         }
     }
-    else
-    {
-        res.render('profile/change_password',{wrongPw:true})
-    }
-    
-
 });
 
 module.exports = route;
